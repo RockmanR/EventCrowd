f048afdc445cb84c6670c6d3beb6df0168b93853
Ahmed Al Mutawa <rockmanr@gmail.com>
2018-07-16 11:17:17 +0200

    contracts and js updated to fix issues raised with compile and migrate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,17 @@
 var TutorialToken = artifacts.require("TutorialToken");
-const GustavoCoinCrowdsale = artifacts.require('./GustavoCoinCrowdsale.sol');
+const RefundableGCC = artifacts.require('./RefundableGCC.sol');
 const GustavoCoin = artifacts.require('./GustavoCoin.sol');
 
 
 module.exports = function(deployer, network, accounts) {
-    const openingTime = web3.eth.getBlock('latest').timestamp + 5; // two secs in the future
-    const closingTime = openingTime + 60*60*24*1; // one day in the future (60 sec * 60 min * 24 hours * 1 day)
+    const openingTime = web3.eth.getBlock('latest').timestamp + 30; // five secs in the future
+    const closingTime = openingTime + 60*10; // one day in the future (60 sec * 10 min)
     const rate = new web3.BigNumber(1000);
     const wallet = accounts[1];
+    const goal = 5000000000000000000; // 5 ether
 
     return deployer
         .then(() => { return deployer.deploy(GustavoCoin); })
-        .then(() => { return deployer.deploy(GustavoCoinCrowdsale,openingTime,closingTime,rate,wallet,GustavoCoin.address); })
+        .then(() => { return deployer.deploy(RefundableGCC,openingTime,closingTime,rate,wallet,GustavoCoin.address,goal); })
         .then(() => { return deployer.deploy(TutorialToken); });
 };
